0d0ecb48717a85e8ac3d6720bb2c0215ce54b283
Xu Yi <widebluesky@qq.com>
2016-12-05 22:31:54 +0800

    delete

--- config/Constant.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-"Constant Module"
-IS_TEST = True
-GIT_REPOSITORY_NAME = "RemoteConfig"
-GIT_LOCAL_PATH = "/Users/xuyi/Documents/document/git"
-GIT_LOCAL_REPOSITORY_PATH = GIT_LOCAL_PATH + "/" + GIT_REPOSITORY_NAME
-GIT_REMOTE_URL = "https://github.com/widebluesky/" + GIT_REPOSITORY_NAME + ".git"
-GIT_ACCOUNT = ""
-GIT_PASSWORD = ""
