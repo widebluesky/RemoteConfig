ebc03b5866f83b50b052dc61ec6466ca1d878ec3
Xu Yi <widebluesky@qq.com>
2016-12-16 19:07:39 +0800

    add get a new internet ip method

--- util/SocketUtil.py
+++ util/SocketUtil.py
@@ -23,6 +23,30 @@ class SocketUtil(object):
         html = response.read()
         ip_address = re.search(r'code.(.*?)..code', html)
         return ip_address.group(1)
+    def get_local_ip_address(self):
+        "Get Local IP Address Method"
+        try:
+            csock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            csock.connect(('8.8.8.8', 80))
+            (addr, port) = csock.getsockname()
+            csock.close()
+            return addr
+        except socket.error:
+            return "127.0.0.1"
+    def get_internet_ip_address2(self):
+        try:
+            myip = self.visit("http://www.whereismyip.com/")
+        except:
+            try:
+                myip = self.visit("http://www.ip138.com/ip2city.asp")
+            except:
+                myip = "So sorry!!!"
+        return myip
+    def visit(self,url):
+        opener = urllib2.urlopen(url)
+        if url == opener.geturl():
+            str = opener.read()
+        return re.search('\d+\.\d+\.\d+\.\d+',str).group(0)
 
 #class Main(object):
 #    "Main Class"
