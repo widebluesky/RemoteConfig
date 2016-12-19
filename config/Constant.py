3a27e2c06a422ee541027779f7eaa1d371d8f552
Yi Xu <widebluesky@qq.com>
2016-12-19 10:16:52 +0800

    Update Constant.py

--- config/Constant.py
+++ config/Constant.py
@@ -6,4 +6,4 @@ GIT_REPOSITORY_NAME = "RemoteConfig"
 GIT_LOCAL_PATH = "/Users/xuyi/Documents/document/git"
 GIT_LOCAL_REPOSITORY_PATH = GIT_LOCAL_PATH + "/" + GIT_REPOSITORY_NAME
 GIT_LOCAL_CONFIG_FILE_PATH = GIT_LOCAL_REPOSITORY_PATH + '/config.json'
-GIT_REMOTE_URL = "git://github.com/widebluesky/" + GIT_REPOSITORY_NAME + ".git"
+GIT_REMOTE_URL = "https://github.com/widebluesky/" + GIT_REPOSITORY_NAME + ".git"
